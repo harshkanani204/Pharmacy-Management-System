bce8f8bc6d9813fa16934eb9a7be4648bd126ba7
VILLGAXOP <80449929+VILLGAXOP@users.noreply.github.com>
2023-04-14 17:03:18 +0530

    add supplier and supplier details

--- PMS/admin/add-supplier.php
+++ PMS/admin/add-supplier.php
@@ -1,76 +1,104 @@
+<?php
+extract($_POST);
+
+
+$conn = new mysqli('localhost', 'root', '', 'pms');
+echo "ssds";
+if ($conn->connect_error) {
+  echo ('connection failed : ' . $conn->connect_error);
+} else {
+  $stmt = $conn->prepare("insert into supplier (ID, Name, Email, Phone, Address) VALUES (?,?, ?, ?, ?)");
+
+  $stmt->bind_param("issis", $ID, $Name, $Email, $Phone, $Address);
+  $stmt->execute();
+
+  $stmt->close();
+  $conn->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'include/header.php';?>
+<?php include 'include/header.php'; ?>
 
-  <body class="nav-md">
-            <?php include 'include/sidebar.php';?>
-            <?php include 'include/menufooter.php';?>
-          </div>
+<body class="nav-md">
+  <?php include 'include/sidebar.php'; ?>
+  <?php include 'include/menufooter.php'; ?>
+  </div>
+  </div>
+
+  <?php include 'include/topnav.php'; ?>
+
+  <!-- page content -->
+  <div class="right_col" role="main">
+    <div class="">
+      <div class="page-title">
+        <div class="title_left">
+          <h3><i class="fa fa-truck"></i> Add Supplier</h3>
         </div>
+      </div>
 
-        <?php include 'include/topnav.php';?>
+      <div class="clearfix"></div>
 
-        <!-- page content -->
-        <div class="right_col" role="main">
-          <div class="">
-            <div class="page-title">
-              <div class="title_left">
-                <h3><i class="fa fa-truck"></i> Add Supplier</h3>
-              </div>
+      <div class="row">
+        <div class="col-md-12 col-sm-12  ">
+          <div class="x_panel">
+            <div class="x_title">
+              <h2>Supplier Information</h2>
+              <div class="clearfix"></div>
             </div>
-
-            <div class="clearfix"></div>
-
-            <div class="row">
-              <div class="col-md-12 col-sm-12  ">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Supplier Information</h2>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                  <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
-                  <div class="item form-group">
-                  <div class="col-md-8 col-sm-8 offset-md-2">
-                    <input type="text" class="form-control has-feedback-left" placeholder="Supplier Name">
-                    <span class="fa fa-truck form-control-feedback left" aria-hidden="true"></span>
-                  </div>
-                </div>
+            <div class="x_content">
+              <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" method="post">
                 <div class="item form-group">
                   <div class="col-md-8 col-sm-8 offset-md-2">
-                    <input type="text" class="form-control has-feedback-left" placeholder="ex. 09809879797">
+                    <input type="text" class="form-control has-feedback-left" placeholder="ex. 09809879797" name="ID">
                     <span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
                   </div>
                 </div>
                 <div class="item form-group">
                   <div class="col-md-8 col-sm-8 offset-md-2">
-                    <input type="text" class="form-control has-feedback-left" placeholder="ex. supplier@gmail.com">
+                    <input type="text" class="form-control has-feedback-left" placeholder="Supplier Name" name="Name">
+                    <span class="fa fa-truck form-control-feedback left" aria-hidden="true"></span>
+                  </div>
+                </div>
+                <div class="item form-group">
+                  <div class="col-md-8 col-sm-8 offset-md-2">
+                    <input type="text" class="form-control has-feedback-left" placeholder="ex. supplier@gmail.com" name="Email">
                     <span class="fa fa-envelope form-control-feedback left" aria-hidden="true"></span>
                   </div>
                 </div>
                 <div class="item form-group">
                   <div class="col-md-8 col-sm-8 offset-md-2">
-                    <input type="text" class="form-control has-feedback-left" placeholder="Address">
+                    <input type="text" class="form-control has-feedback-left" placeholder="ex. 09809879797" name="Phone">
+                    <span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
+                  </div>
+                </div>
+                <div class="item form-group">
+                  <div class="col-md-8 col-sm-8 offset-md-2">
+                    <input type="text" class="form-control has-feedback-left" placeholder="Address" name="Address">
                     <span class="fa fa-map form-control-feedback left" aria-hidden="true"></span>
                   </div>
                 </div>
-                
+
                 <div class="item form-group">
                   <div class="col-md-8 col-sm-8 offset-md-2">
-                      <button class="btn btn-primary" type="button">Cancel</button>
-                      <button type="submit" class="btn btn-success">Submit</button>
+                    <button class="btn btn-primary" type="button">Cancel</button>
+                    <button type="submit" class="btn btn-success">Submit</button>
                   </div>
                 </div>
-                  </form>
-                  </div>
-                </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
+  </div>
+  </div>
 
-    <?php include 'include/footer.php';?>
-  </body>
+
+
+  <?php include 'include/footer.php'; ?>
+</body>
+
 </html>
--- PMS/admin/supplier.php
+++ PMS/admin/supplier.php
@@ -1,50 +1,113 @@
+<!-- PHP code to fetch supplier information -->
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "pms";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+echo "connection was successful";
+                      
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch supplier information
+$sql = "SELECT * FROM supplier"; // Modify the query to suit your database schema
+$result = $conn->query($sql);
+
+// Check if query was successful
+if ($result === false) {
+    die("Error: " . $conn->error);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'include/header.php';?>
+<?php include 'include/header.php'; ?>
 
-  <body class="nav-md">
-            <?php include 'include/sidebar.php';?>
-            <?php include 'include/menufooter.php';?>
-          </div>
+<body class="nav-md">
+  <?php include 'include/sidebar.php'; ?>
+  <?php include 'include/menufooter.php'; ?>
+  </div>
+  </div>
+
+  <?php include 'include/topnav.php'; ?>
+
+  <!-- page content -->
+  <div class="right_col" role="main">
+    <div class="">
+      <div class="page-title">
+        <div class="title_left">
+          <h3><i class="fa fa-truck"></i> Supplier</h3>
         </div>
+      </div>
 
-        <?php include 'include/topnav.php';?>
+      <div class="clearfix"></div>
 
-        <!-- page content -->
-        <div class="right_col" role="main">
-          <div class="">
-            <div class="page-title">
-              <div class="title_left">
-                <h3><i class="fa fa-truck"></i> Supplier</h3>
-              </div>
+      <div class="row">
+        <div class="col-md-12 col-sm-12  ">
+          <div class="x_panel">
+            <div class="x_title">
+              <h2>List of Suppliers</h2>
+              <ul class="nav navbar-right panel_toolbox">
+                <a href="add-supplier.php" class="btn btn-sm btn-info text-white"><i class="fa fa-plus"></i> Add Supplier</a>
+              </ul>
+              <div class="clearfix"></div>
             </div>
+            <div class="x_content">
+              <table id="datatable" class="table table-striped table-bordered" style="width:100%">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
 
-            <div class="clearfix"></div>
 
-            <div class="row">
-              <div class="col-md-12 col-sm-12  ">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>List of Suppliers</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                    <a href="add-supplier.php" class="btn btn-sm btn-info text-white"><i class="fa fa-plus"></i> Add Supplier</a>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                  <table id="datatable" class="table table-striped table-bordered" style="width:100%">
-                      <thead>
-                        <tr>
-                          <th>Supplier Name</th>
-                          <th>Contact</th>
-                          <th>Email</th>
-                          <th>Complete Address</th>
-                          <th>Action</th>
+                <tbody>
+
+                  <?php
+                  // Process result
+                  if ($result->num_rows > 0) {
+                    // Output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                      echo "<tr>";
+                      echo "<td>" . $row["ID"] . "</td>";
+                      echo "<td>" . $row["Name"] . "</td>";
+                      echo "<td>" . $row["Email"] . "</td>";
+                      echo "<td>" . $row["Phone"] . "</td>";
+                      echo "<td>" . $row["Address"] . "</td>";
+                      echo '<td>
+                    <a class="btn btn-sm btn-success text-white"><i class="fa fa-edit"></i> edit</a>
+                    <a class="btn btn-sm btn-danger text-white"><i class="fa fa-trash"></i> delete</a>
+                  </td>';
+                      echo "</tr>";
+                    }
+                  } else {
+                    echo "<tr><td colspan='5'>0 results</td></tr>";
+                  }
+
+                  // Close database connection
+                  $conn->close();
+                  ?>
+                  <!-- <tr>
+                          <td>Supplier 1</td>
+                          <td>+6392978376192</td>
+                          <td>supplier1@gmail.com</td>
+                          <td>456 Boni Av. Mandaluyong, Metro Manila</td>
+                          <td>
+                              <a class="btn btn-sm btn-success text-white"><i class="fa fa-edit"></i> edit</a>
+                              <a class="btn btn-sm btn-danger text-white"><i class="fa fa-trash"></i> delete</a>
+                          </td>
                         </tr>
-                      </thead>
-
-
-                      <tbody>
                         <tr>
                           <td>Supplier 1</td>
                           <td>+6392978376192</td>
@@ -55,6 +118,7 @@
                               <a class="btn btn-sm btn-danger text-white"><i class="fa fa-trash"></i> delete</a>
                           </td>
                         </tr>
+                        <tr>
                         <tr>
                           <td>Supplier 2</td>
                           <td>+6396748976521</td>
@@ -94,18 +158,19 @@
                               <a class="btn btn-sm btn-success text-white"><i class="fa fa-edit"></i> edit</a>
                               <a class="btn btn-sm btn-danger text-white"><i class="fa fa-trash"></i> delete</a>
                           </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
+                        </tr> -->
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
+  </div>
+  </div>
 
-    <?php include 'include/footer.php';?>
-  </body>
+  <?php include 'include/footer.php'; ?>
+</body>
+
 </html>
