95a91a1da9fdf665288700e29033374ca00837e2
harshkanani204 <harshkanani204@gmail.com>
2023-05-02 00:55:38 +0530

    changed password

--- pharmacy/pharmacy/settings.py
+++ pharmacy/pharmacy/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'database',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
