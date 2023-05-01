d28f9aabfde8ba32eb02acdeaca4410adfbf6638
harshkanani204 <harshkanani204@gmail.com>
2023-05-02 01:20:35 +0530

    models

--- pharmacy/pharmacyapp/admin.py
+++ pharmacy/pharmacyapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Company,Customer,Drug,Drugexpiry,Employee,Inventory,Login,Prescription,Purchase,Sales,Supplier
 
 # Register your models here.
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Company._meta.get_fields()]
+
+admin.site.register(Company, CompanyAdmin)
